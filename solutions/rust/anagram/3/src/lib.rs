8e387a85e8e4fd9f098f707cd7d189d5ac63f946
Stefan Fleiter <stefan.fleiter+gh@gmail.com>
2025-09-20 20:26:32 +0200

    anagram: refactoring and adding adding tests

--- solutions/rust/anagram/3/src/lib.rs
+++ solutions/rust/anagram/3/src/lib.rs
@@ -13,8 +13,8 @@ fn check_anagram(word_graphemes_sorted: &Vec<&str>, candidate: &str) -> bool {
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let word_lc = word.to_lowercase();
-    let mut results: HashSet<&str> = HashSet::new();
     let word_graphemes_sorted: Vec<&str> = to_sorted_graphemes(&word_lc);
+    let mut results: HashSet<&str> = HashSet::new();
     for candidate in possible_anagrams {
         let candidate_lc = candidate.to_lowercase();
         if word_lc != candidate_lc && check_anagram(&word_graphemes_sorted, &candidate_lc) {
